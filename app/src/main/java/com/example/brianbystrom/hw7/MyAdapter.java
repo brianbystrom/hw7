281ef23bc3494aa8be9e7be6962e5b0680132ca6
salad7 <msalad@uncc.edu>
2017-03-12 14:23:34 -0400

    Added date converter

--- app/src/main/java/com/example/brianbystrom/hw7/MyAdapter.java
+++ app/src/main/java/com/example/brianbystrom/hw7/MyAdapter.java
@@ -52,9 +52,41 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> implem
 
     private int convertMonthToNumber(String s){
         if(s.equals("Jan")){
-            return 01;
+            return 1;
         }
-
+        else  if(s.equals("Feb")){
+            return 2;
+        }
+        else  if(s.equals("Mar")){
+            return 3;
+        }
+        else  if(s.equals("Apr")){
+            return 4;
+        }
+        else  if(s.equals("May")){
+            return 5;
+        }
+        else  if(s.equals("Jun")){
+            return 6;
+        }
+        else  if(s.equals("July")){
+            return 7;
+        }
+        else  if(s.equals("Aug")){
+            return 8;
+        }
+        else  if(s.equals("Sep")){
+            return 9;
+        }
+        else  if(s.equals("Jan")){
+            return 10;
+        }
+        else  if(s.equals("Jan")){
+            return 11;
+        }
+        else  if(s.equals("Jan")){
+            return 12;
+        }
         return 99;
     }
 
